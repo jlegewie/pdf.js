5fbcc27c869d9e3e6a94d2d0560175816069f96a
Joscha Legewie <jpl2136@columbia.edu>
2013-04-22 17:51:55 -0400

    example: extract annotations (skip pages without annotations etc)

--- examples/extract/extract.js
+++ examples/extract/extract.js
@@ -1,75 +1,64 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
-//
-// See README for overview
-//
-
 'use strict';
 
-// PROBLEM: hard3, hard4, hard6
-
-// https://github.com/mozilla/pdf.js/pull/1119
-// cd '/Users/jlegewie/Documents/coding/pdf.js'
+// example to extract annotations
+// cd to pdf.js folder
 // python -m SimpleHTTPServer 8888
 // http://localhost:8888/examples/extract/index.html
-// http://localhost:8888/zotfile/chrome/content/zotfile/pdfextract/extract.html
 
-  
+const SUPPORTED_ANNOTS = ["Text", "Highlight", "Underline"]
+
 // Fetch the PDF document from the URL using promices
-//
-// PDFJS.getDocument('test2.pdf').then(function(pdf) {
-PDFJS.getDocument('/examples/extract/pdf/hard3.pdf').then(function(pdf) {
-  var annotations = [];
-  // Using promise to fetch the page
+PDFJS.getDocument('/examples/extract/pdf/test2.pdf').then(function(pdf) {
+  var annotations = [],
+      numPages = pdf.numPages;
 
+  // function to handle page (render and extract annotations)
   var extract = function(page) {    
       var scale = 1;
       var viewport = page.getViewport(scale);
 
-      //
       // Prepare canvas using PDF page dimensions
-      //
       var canvas = document.getElementById('the-canvas');
       var context = canvas.getContext('2d');
       canvas.height = viewport.height;
       canvas.width = viewport.width;
-
-      //
       // Render PDF page into canvas context
-      //
       var renderContext = {
         canvasContext: context,
         viewport: viewport
       };
 
-      page.render(renderContext).then(function(x) {
-        // annotations
-        page.getAnnotations().then(function extractAnno(annotations) {        
-          var annos = annotations.filter(function(anno) {return anno.type!="Link";});
-          console.log("PAGE: " + page.pageNumber);
-          console.log(annos.map(function(anno) {return anno.markup;}));
-          console.log(annos.map(function(anno) {return anno.content;}));
-          //console.log(annotations.filter(function(anno) {return anno.type=="Highlight";}));
-
-          if(numPages>page.pageNumber) return pdf.getPage(page.pageNumber+1).then(extract);
-          else {
-            return ;
-          }
-          
+      // get annotations
+      var annotations;
+      page.getAnnotations().then(function extractAnno(annos) {
+        // filter for supported annotations
+        annotations = annos.filter(function(anno) {return SUPPORTED_ANNOTS.indexOf(anno.type) >= 0;});
+        // skip page if there is nothing interesting
+        if (annotations.length==0) return;
+        // render page
+        page.render(renderContext).then(function() {
+          // show annotations
+          // console.log(annos.map(function(anno) {return anno;}));
+          console.log(annotations.map(function(anno) {return anno.markup;}));
+          console.log(annotations.map(function(anno) {return anno.content;}));
+          // render next page
+          if(numPages>page.pageNumber) pdf.getPage(page.pageNumber+1).then(extract);
+        },
+        // error handler for page
+        function(error) {          
+          // render next page
+          if(numPages>page.pageNumber) pdf.getPage(page.pageNumber+1).then(extract);
         });
-        
-      });
-      /*page.getTextContent().then(function test(content) {
-        console.log(content);
-      });*/
 
+      });
     };
-  var numPages = pdf.numPages;
+
+  // Using promise to fetch the page
   pdf.getPage(1).then(extract);
-  // for (var i = 1; i <= numPages; i++) {
-  //   pdf.getPage(i).then(action);
-  // }
+
 });
 
 
